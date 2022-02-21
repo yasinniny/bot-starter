476ea882e3ef323a5798b06fa31aa65eb55f42e0
Janghyon <prog.kawaii00@gmail.com>
2022-02-22 05:50:50 +0900

    fixed index.js there was not 'interactionCreate'

--- index.js
+++ index.js
@@ -26,4 +26,18 @@ for (const file of eventFiles) {
     }
 }
 
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if(!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch(error) {
+        console.error(error);
+    }
+})
+
 client.login(token);
