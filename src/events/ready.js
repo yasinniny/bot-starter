0c0e75e9a049841191fda73669e133fe52f72c4f
yasinniny <prog.kawaii00@gmail.com>
2022-03-08 07:45:05 +0900

    change: client.user.id -> username

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log(`The bot is on you are currently using ${client.user.id}`);
+        console.log(`The bot is on you are currently using ${client.user.username}`);
     }
 }
